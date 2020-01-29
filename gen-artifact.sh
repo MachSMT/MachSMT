9dff79441bc7aa2274c3c04011be8d689700fd94
Mathias Preiner <mathias.preiner@gmail.com>
2020-01-28 22:29:36 -0800

    Ensure a clean repo for the artifact.

--- gen-artifact.sh
+++ gen-artifact.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
 
-tar cJf cavae20-artifact.tar.xz \
-  Dockerfile \
-  README-artifact.md \
-  artifact-data.tar.xz \
-  bin \
-  demo.sh \
-  machsmt \
-  requirements.txt \
-  setup.py
 
-sha1sum cavae20-artifact.tar.xz
+CLEAN_DIR="/tmp/machsmt-$$"
+DIR=machsmt-artifact
+
+git clone https://github.com/j29scott/MachSMT.git "$CLEAN_DIR"
+
+TO_COPY="
+Dockerfile
+README-artifact.md
+README.md
+artifact-data.tar.xz
+bin
+demo.sh
+machsmt
+requirements.txt
+setup.py
+"
+
+rm -rf "$DIR"
+mkdir "$DIR"
+
+# Copy files from fresh git clone
+for f in $TO_COPY; do
+  cp -r "$CLEAN_DIR/$f" "$DIR"
+done
+
+tar cJf machsmt-artifact.tar.xz "$DIR"
+sha1sum machsmt-artifact.tar.xz
+
+rm -rf "$DIR"
+rm -rf "$CLEAN_DIR"

93c06f43f1e2aea2bbb14c4fb4c1c8fe9845225b
Aina Niemetz <aina.niemetz@gmail.com>
2020-09-16 14:31:33 -0700

    extra features: Add arrays.py.

---
+++ machsmt/features/arrays.py
@@ -0,0 +1,20 @@
+
+'''
+To add an extra feature, define a function anywhere in this module/file.
+
+All functions that appear in this model will be automatically included in the ML model.
+
+INPUT
+The function siguature should include the following:
+
+    file_path - absolute path to smt2 input as string
+
+OUTPUT
+
+    A single number or an iterable of numbers
+
+'''
+
+# TODO
+#  - avg/mean reads per array
+#  - avg/mean depth of store chains

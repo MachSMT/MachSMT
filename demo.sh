e9f7ab4732dffd39e282ad50baa9e9775832a928
joe <j29scott@uwaterloo.ca>
2020-10-23 13:38:18 -0400

    only do bv by default

--- demo.sh
+++ demo.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
+build_all=
+usage() {
+cat << EOF
+usage: $0 [-h|-a]
+-h       print this message and exit
+-a       build models and generate data for BV, NRA, QF_UFBV, QF_LIA, and QF_BVFPLRA
+By default $0 builds the model and generates the data for BV only.
+EOF
+  exit 0
+}
+
+while [ $# -gt 0 ]
+do
+  opt=$1
+  case $opt in
+      -h|--help) usage;;
+      -a) build_all=yes;;
+ esac
+ shift
+done
+
+files="smt-comp-data/bv.csv"
+if [ -n "$build_all" ]; then
+  files="smt-comp-data/bv.csv smt-comp-data/nra.csv smt-comp-data/qf_bvfplra.csv smt-comp-data/qf_lia.csv smt-comp-data/qf_ufbv.csv"
+fi
+
+
 # 1) Build and evaluate learned models.
 #
 # The build process creates the lib directory which contains 
 # final learnt models for machsmt. This process also performs
 # all preprocessing steps for evaluation
 
-machsmt_build -f smt-comp-data/smtcomp_2019.csv smt-comp-data/smtcomp_2020.csv -l lib
+machsmt_build -f $files -l lib
 
 # 2) Evaluate MachSMT
 # This process evaluates machsmt under kfold cross validation

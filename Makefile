b70a12006599187616335b078fa0292756a96ea0
j29scott <j29scott@uwaterloo.ca>
2021-08-23 18:03:52 -0400

    missing files

---
+++ Makefile
@@ -0,0 +1,11 @@
+install:
+	pip3 install -r requirements.txt
+	sudo python3 setup.py develop #TODO: make this less invasive. 
+	make test
+
+test:
+	python3 tests/regress/main_test.py
+
+lint:
+	autopep8 bin --in-place --recursive --max-line-length 100 --experimental -a -a -a -a -a
+	pylint machsmt --exit-zero
--- setup.py
+++ setup.py
@@ -3,26 +3,25 @@
 import os
 from setuptools import setup, find_packages
 setup(
-    name         = 'machsmt',
-    version      = '0.3',
-    description  = 'An algorithm selection tool for SMT-LIB solvers',
-    author       = 'Joe Scott, Aina Niemetz, Mathias Preiner, Vijay Ganesh',
-    author_email = 'joseph.scott@uwaterloo.ca',
-    url          = 'https://github.com/j29scott/smtzilla',
-    scripts      = [
+    name='machsmt',
+    version='1.0',
+    description='An algorithm selection tool for SMT-LIB solvers',
+    author='Joe Scott, Aina Niemetz, Mathias Preiner, Saeed Nejati, and Vijay Ganesh',
+    author_email='joseph.scott@uwaterloo.ca',
+    url='https://github.com/j29scott/machsmt',
+    scripts=[
         'bin/machsmt_build',
         'bin/machsmt_eval',
-        'bin/machsmt_eval_smtcomp',
         'bin/machsmt',
     ],
-    packages     = find_packages(),
-    package_dir  = {
+    packages=find_packages(),
+    package_dir={
         'machsmt': 'machsmt',
     },
     install_requires=[
         'matplotlib',
         'progress',
         'scikit-learn',
-	'func_timeout',
+        'func_timeout',
     ],
 )
